58ff9db4adc32cad0ddce04ff18a3d058a7feadf
Fairooz14 <fairoozcse@gmail.com>
2024-10-18 17:43:44 +0600

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String result = '0';
+  String value = '0';
+  double number1 = 0.0;
+  double number2 = 0.0;
+  String operand = '';
+
+  //function to calculate the result
+  calculate(dynamic buttonText) 
+  {
+    if (buttonText == "AC") {
+      result = '0';
+      value = '0';
+      number1 = 0.0;
+      number2 = 0.0;
+      operand = '';
+    } else if (buttonText == "Del") {
+      result = '0';
+      value = '0';
+      number1 = 0.0;
+      number2 = 0.0;
+      operand = '';
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "*" ||
+        buttonText == "/") 
+        {
+      number1 = double.parse(value);
+      operand = buttonText;
+      value = '0';
+    } else if (buttonText == "=") {
+      number2 = double.parse(result);
+      if (operand == "+") {
+        value = (number1 + number2).toString();
+      }
+      if (operand == "-") {
+        value = (number1 - number2).toString();
+      }
+      if (operand == "*") {
+        value = (number1 * number2).toString();
+      }
+      if (operand == "/") {
+        value = (number1 / number2).toString();
+      }
+
+      number1 = 0.0;
+      number2 = 0.0;
+      operand = '';
+    } else {
+      value = value + buttonText;
+    }
+
+    setState(() {
+      result = double.parse(result).toStringAsFixed(2);
+    });
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //=============== App Bar =================
+      appBar: AppBar(
+        title: Center(
+          child: const Text(
+            'Basic Calculator',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 208, 106, 159),
+      ),
+
+      //=============== Body =================
+      body: Column(
+        children: [
+          //=============== Calculator UI =================
+          Container(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '0.00',
+                style: TextStyle(fontSize: 40),
+              ),
+            ),
+          ),
+
+          Expanded(child: Divider()),
+
+          Column(
+            children: [
+              Row(
+                children: [
+                  buildButton('AC'),
+                  buildButton('%'),
+                  buildButton('Del'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('7'),
+                  buildButton('8'),
+                  buildButton('9'),
+                  buildButton('/'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('4'),
+                  buildButton('5'),
+                  buildButton('6'),
+                  buildButton('*'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('1'),
+                  buildButton('2'),
+                  buildButton('3'),
+                  buildButton('-'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('.'),
+                  buildButton('0'),
+                  buildButton('='),
+                  buildButton('+'),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//=============== Button Building =================
+Widget buildButton(String buttonText) {
+  return Expanded(
+    child: Container(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 225, 140, 168),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: Colors.white),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(buttonText,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              )),
+        ),
+        onPressed: () {
+          print(buttonText);
+          calculate(buttonText);
+        },
+      ),
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:everything_is_widget_2/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
