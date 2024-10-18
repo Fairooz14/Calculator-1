56ed356960afdc0f16e2d521784a41d525c3330c
Fairooz14 <fairoozcse@gmail.com>
2024-10-18 22:10:57 +0600

    completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -80,7 +80,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Center(
           child: const Text(
-            'Basic Calculator',
+            'The Pink Calculator',
             style: TextStyle(color: Colors.white),
           ),
         ),
